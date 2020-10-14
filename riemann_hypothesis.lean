f199de3eb935408058b22ec65427a3216dc25498
AlexKontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2020-10-14 14:41:38 -0400

    Add files via upload

---
+++ riemann_hypothesis.lean
@@ -0,0 +1,29 @@
+/- ------------------------------------------------------------------------- -|
+| @project: riemann_hypothesis                                                |
+| @file:    riemann_hypothesis.lean                                           |
+| @author:  Brandon H. Gomes and Alex Kontorovich                                                  |
+| @affil:   Rutgers University                                                |
+|- ------------------------------------------------------------------------- -/
+
+import .dirichlet_eta
+
+/-!
+-/
+
+namespace riemann_hypothesis --——————————————————————————————————————————————————————————--
+variables {ℂ : Type*} {ℝ : Type*}
+          [has_lift_t nat ℝ] [has_lift_t ℝ ℂ] [preorder ℝ] [Algebra ℝ] [Algebra ℂ]
+          (ℭ : Complex ℂ ℝ)
+
+open algebra
+
+/--
+-/
+def form.dirichlet_eta
+    [has_lift_zero_same nat ℝ]
+    [has_lift_lt_comm nat ℝ]
+    (s) (σpos : 0 < ℭ.real_part s)
+    (σ_le_one : ℭ.real_part s < 1)
+    := is_convergent ℭ.abs (dirichlet_eta.partial_pairs ℭ s) 0 → ℭ.real_part s = 2⁻¹
+
+end riemann_hypothesis --————————————————————————————————————————————————————————————————--
